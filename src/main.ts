534b5d1fe8f30e13a71ba96c3b46be970f062544
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2022-12-09 22:06:21 +0100

    Added button to spin wheel

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './style.css';
+
+let degree = 0;
+
+document.querySelector('#spin-button')?.addEventListener('click', () => {
+  const spinner = document.querySelector('.wheel-container') as HTMLDivElement;
+  if (!spinner) return;
+
+  spinner.style.transform = `rotate(${degree}deg)`;
+  degree += Math.random() * 1000;
+});
