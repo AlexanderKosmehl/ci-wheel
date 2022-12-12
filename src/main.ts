8e989f05a4ac72eeb5605ad569b35b212d3f6f4c
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2022-12-12 11:45:54 +0100

    Added list component

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,57 @@
 import generateWheel from './components/wheel';
 import './style.css';
 
+const wheelContainer = document.querySelector<HTMLDivElement>('#wheel-container');
+const listElement = document.querySelector<HTMLUListElement>('#list');
+const listInput = document.querySelector<HTMLInputElement>('#list-input');
+const inputButton = document.querySelector<HTMLButtonElement>('#list-input-button');
+const spinButton = document.querySelector<HTMLButtonElement>('#spin-button');
+
 let degree = 0;
+const listEntries: string[] = [];
 
-const wheelContainer = document.getElementById('wheel-container');
-wheelContainer?.appendChild(generateWheel(Object.keys(new Array(16).fill(0))));
+function renderWheel(labels: string[]) {
+  if (!wheelContainer || !labels) return;
 
-document.querySelector('#spin-button')?.addEventListener('click', () => {
+  wheelContainer.textContent = '';
+  wheelContainer.appendChild(generateWheel(labels));
+
+  degree = 0;
+}
+
+spinButton?.addEventListener('click', () => {
   const spinner = document.querySelector('.wheel') as HTMLDivElement;
   if (!spinner) return;
 
-  degree += Math.random() * 1000;
+  degree += 720 + Math.random() * 360;
   spinner.style.transform = `rotate(${degree}deg)`;
 });
+
+function renderList(labels: string[]) {
+  if (!listElement) return;
+
+  listElement.textContent = '';
+
+  labels.forEach((label) => {
+    const newListEntry = document.createElement<'li'>('li');
+    newListEntry.textContent = label;
+    listElement.appendChild(newListEntry);
+  });
+}
+
+function handleInput() {
+  if (!listInput) return;
+
+  listEntries.push(listInput.value);
+  listInput.value = '';
+
+  renderList(listEntries);
+  renderWheel(listEntries);
+}
+
+listInput?.addEventListener('keydown', (event: KeyboardEvent) => {
+  if (event.key !== 'Enter') return;
+
+  handleInput();
+});
+inputButton?.addEventListener('click', handleInput);
