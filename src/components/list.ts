3d26dd6ca9ec29c1ea811d16f38d0cf587629a24
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2022-12-14 11:37:33 +0100

    Added styling to list

--- src/components/list.ts
+++ src/components/list.ts
@@ -1,6 +1,27 @@
+import styles from './list.module.css';
+
+const listContainer = document.querySelector<HTMLDivElement>('#list-container');
+listContainer?.classList.add(styles.listContainer);
+
 const listElement = document.querySelector<HTMLUListElement>('#list');
+listElement?.classList.add(styles.list);
+
+const inputBar = document.querySelector<HTMLDivElement>('#input-bar');
+inputBar?.classList.add(styles.inputBar);
+
 const listInput = document.querySelector<HTMLInputElement>('#list-input');
+listInput?.classList.add(styles.input);
+
 const inputButton = document.querySelector<HTMLButtonElement>('#list-input-button');
+inputButton?.classList.add(styles.inputButton);
+
+function generateListElement(label: string) {
+  const elementContainer = document.createElement<'div'>('div');
+  elementContainer.classList.add(styles.elementContainer);
+  elementContainer.textContent = label;
+
+  return elementContainer;
+}
 
 export default class ListComponent {
   listEntries: string[];
@@ -28,7 +49,8 @@ export default class ListComponent {
 
     this.listEntries.forEach((entry, entryIndex) => {
       const newListEntry = document.createElement<'li'>('li');
-      newListEntry.textContent = entry;
+      newListEntry.classList.add(styles.listEntry);
+      newListEntry.appendChild(generateListElement(entry));
       newListEntry.addEventListener('click', () => {
         this.listEntries.splice(entryIndex, 1);
 
