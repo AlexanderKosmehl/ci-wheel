5daea21fc5c7ae07a2aaac059a794fcf70394d36
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2022-12-20 11:24:50 +0100

    Added configuration to mock css modules

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|svg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.cjs",
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
 };
