d945cd415afb0abbe89f825553c2b2eb353349c2
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2022-12-29 12:04:45 +0100

    Added tests for modalBase and

---
+++ src/components/modal/modalBase/modalBase.test.ts
@@ -0,0 +1,30 @@
+import {
+  describe, expect, it, jest,
+} from '@jest/globals';
+import generateModal from './modalBase';
+
+describe('generateModal', () => {
+  const content = document.createElement<'div'>('div');
+  content.textContent = 'Content';
+
+  const onClose = jest.fn(() => {});
+
+  const modal = generateModal({
+    title: 'Title',
+    content,
+    onClose,
+  });
+
+  const closeButton = modal.querySelector<HTMLButtonElement>('.closeIcon');
+
+  it('generates component correctly', () => {
+    expect(modal).toMatchSnapshot();
+  });
+
+  it('adds working onClose callback', () => {
+    if (!closeButton) throw Error('No closeButton rendered!');
+
+    closeButton.click();
+    expect(onClose).toBeCalled();
+  });
+});
