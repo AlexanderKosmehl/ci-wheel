03a290a6d0a6729c817ec28853e5db1f2eeff0cb
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2023-01-03 11:23:52 +0100

    Updated css to fit css-sanitize

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
 import './global.css';
 
 import { getSearchParams } from './util/searchParamHelper';
