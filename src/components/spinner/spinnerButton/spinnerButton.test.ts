3f507940092086874e9c38914b4bddea432991d2
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2023-01-09 09:31:00 +0100

    Updated existing tests

--- src/components/spinner/spinnerButton/spinnerButton.test.ts
+++ src/components/spinner/spinnerButton/spinnerButton.test.ts
@@ -7,16 +7,12 @@ describe('generateSpinnerButton', () => {
   const onClick = jest.fn(() => {});
   const buttonComponent = generateSpinnerButton({ onClick });
 
-  const button = buttonComponent.querySelector<HTMLButtonElement>('div > button');
-
   it('generates component correctly', () => {
     expect(buttonComponent).toMatchSnapshot();
   });
 
   it('adds working clickListener', () => {
-    if (!button) throw Error('No button rendered!');
-
-    button.click();
+    buttonComponent.click();
 
     expect(onClick).toBeCalled();
   });
