8ee60e8339ad8b4bb76a5c52d474c84f40c25dff
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2023-01-13 10:55:56 +0100

    Migrated to storybook 7

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,20 +1,20 @@
-const { mergeConfig } = require("vite");
-
+const {
+  mergeConfig
+} = require("vite");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/html",
-  core: {
-    builder: "@storybook/builder-vite",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/html-vite",
+    options: {}
   },
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
   async viteFinal(config) {
     return mergeConfig(config, (await import("../vite.config.js")).default);
   },
+  docs: {
+    autodocs: true
+  }
 };
