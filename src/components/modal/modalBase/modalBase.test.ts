6be0be1b6e1ead996fbb68b2162e016405ed892c
Alexander Kosmehl <alexander.kosmehl@cologne-intelligence.de>
2023-01-17 11:08:50 +0100

    Updated modal tests

--- src/components/modal/modalBase/modalBase.test.ts
+++ src/components/modal/modalBase/modalBase.test.ts
@@ -1,5 +1,5 @@
 import {
-  describe, expect, it, vi,
+  describe, expect, it, vi, beforeEach,
 } from 'vitest';
 import generateModal from './modalBase';
 
@@ -7,24 +7,46 @@ describe('generateModal', () => {
   const content = document.createElement<'div'>('div');
   content.textContent = 'Content';
 
-  const onClose = vi.fn(() => {});
+  let onClose: () => void;
 
-  const modal = generateModal({
-    title: 'Title',
-    content,
-    onClose,
+  let modal: HTMLDivElement;
+
+  beforeEach(() => {
+    onClose = vi.fn(() => {});
+
+    modal = generateModal({
+      title: 'Title',
+      content,
+      onClose,
+    });
   });
-
-  const closeButton = modal.querySelector<HTMLButtonElement>('[data-test=modalCloseButton]');
 
   it('generates component correctly', () => {
     expect(modal).toMatchSnapshot();
   });
 
   it('adds working onClose callback', () => {
+    const closeButton = modal.querySelector<HTMLButtonElement>('[data-test=modalCloseButton]');
     if (!closeButton) throw Error('No closeButton rendered!');
 
+    expect(onClose).not.toBeCalled();
     closeButton.click();
+    expect(onClose).toBeCalled();
+  });
+
+  it('adds onClose callback to Esc-Key', () => {
+    const keyEvent = new KeyboardEvent('keydown', { key: 'Escape' });
+
+    expect(onClose).not.toBeCalled();
+    window.dispatchEvent(keyEvent);
+    expect(onClose).toBeCalled();
+  });
+
+  it('adds onClose callback to modal background', () => {
+    const clickEvent = new MouseEvent('click');
+
+    expect(onClose).not.toBeCalled();
+    modal.dispatchEvent(clickEvent);
     expect(onClose).toBeCalled();
   });
 });
